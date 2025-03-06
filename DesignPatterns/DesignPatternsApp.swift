f848ef0f355f1ff1863c3b8b79a6577600d5653e
Christian Skorobogatow <christiansko@icloud.com>
2025-03-06 23:18:35 +0100

    Initial Commit

---
+++ DesignPatterns/DesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsApp.swift
+//  DesignPatterns
+//
+//  Created by Christian Skorobogatow on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
